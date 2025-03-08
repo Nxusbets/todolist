695b601c0f22d924821ef7b5ed362de665e153f8
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2025-03-08 21:18:03 +0000

    modificaciones

--- todo-list/src/app/layout.tsx
+++ todo-list/src/app/layout.tsx
@@ -3,19 +3,21 @@ import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { UserProvider } from "../contexts/UserContext";
 
-
-
 export const metadata: Metadata = {
   title: "To do list",
   description: "Una app de NxuS",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-      <html lang="en">
-          <body>
-              <UserProvider>{children}</UserProvider>
-          </body>
-      </html>
+    <html lang="en">
+      <body>
+        <UserProvider>{children}</UserProvider>
+      </body>
+    </html>
   );
 }
